be6a1bdd67a293d48458e4c8d2cb337ef83a8e20
Roland Lehmann <rlehmann@pixeltricks.de>
2011-08-03 11:16:07 +0200

    init #ignore codesniffer

---
+++ SilvercartPricerange.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * abstract for price ranges
+ * Customers may get a discount depending on the product quantity they bought.
+ *
+ * @author Roland Lehmann <rlehmann@pixeltricks.de>
+ * @copyright Pixeltricks GmbH
+ * @since 03.08.2011
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+class SilvercartPricerange extends DataObject {
+    
+}
+
---
+++ SilvercartProductPricerangeDecorator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Decorator for SilvercartProduct
+ * Changes the behavior of price getters
+ *
+ * @author Roland Lehmann <rlehmann@pixeltricks.de>
+ * @copyright Pixeltricks GmbH
+ * @since 03.08.2011
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+class SilvercartProductPricerangeDecorator extends DataObjectDecorator {
+    
+}
+
