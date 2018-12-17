9f3a4528a146efd4d4fdd77ea3b43975af37bd85
Sebastian Diel <sdiel@pixeltricks.de>
2018-12-17 17:20:35 +0100

    ENHANCEMENT: Added backwards compatibility to SilverStripe versions < 3.7.

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
 
 SS_Object::add_extension('SilvercartProduct',  'SilvercartGraduatedPriceProduct');
 SS_Object::add_extension('Group',              'SilvercartGraduatedPriceGroupDecorator');
