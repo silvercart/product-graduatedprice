56ecd9ba84735a89f01f45021f5f1f833cfe723a
Sebastian Diel <sdiel@pixeltricks.de>
2019-01-28 14:00:24 +0100

    MINOR: Some PSR-2 updates.

--- code/SilvercartGraduatedPriceProduct.php
+++ code/SilvercartGraduatedPriceProduct.php
@@ -20,30 +20,28 @@
  * @since 06.05.2015
  * @license see license file in modules root directory
  */
-class SilvercartGraduatedPriceProduct extends DataExtension {
-
+class SilvercartGraduatedPriceProduct extends DataExtension
+{
     /**
      * Cache for method "getGraduatedPriceForCustomersGroups".
      *
      * @var SilvercartGraduatedPrice
      */
     protected $graduatedPriceForCustomersGroups = null;
-
     /**
      * Cache for method "getGraduatedPricesForCustomersGroups".
      *
      * @var ArrayList
      */
     protected $graduatedPricesForCustomersGroups = null;
-    
-    /**
+        /**
      * 1:n relationships.
      *
      * @var array
      */
-    public static $has_many = array(
+    private static $has_many = [
         'SilvercartGraduatedPrices' => 'SilvercartGraduatedPrice'
-    );
+    ];
     
     /**
      * decorates the price getter of SilvercartProduct. It updates a products price if
@@ -56,7 +54,8 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      * @author Roland Lehmann <rlehmann@pixeltricks.de>
      * @since 3.8.2011
      */
-    public function updatePrice(SilvercartMoney &$price) {
+    public function updatePrice(SilvercartMoney &$price) : void
+    {
         $customerPrice = $this->getGraduatedPriceForCustomersGroups();
         if ($customerPrice) {
             $price = $customerPrice->price;
@@ -73,12 +72,13 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 16.05.2012
      */
-    public function updateFieldLabels(&$labels) {
+    public function updateFieldLabels(&$labels) : void
+    {
         $labels = array_merge(
                 $labels,
-                array(
+                [
                     'SilvercartGraduatedPrices' => _t('SilvercartGraduatedPrice.PLURALNAME'),
-                )
+                ]
         );
     }
 
@@ -88,43 +88,40 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      * 
      * @return SilvercartGraduatedPrice|false the most convenient price
      */
-    public function getGraduatedPriceForCustomersGroups() {
+    public function getGraduatedPriceForCustomersGroups()
+    {
         if ($this->graduatedPriceForCustomersGroups === null) {
             $member                          = SilvercartCustomer::currentUser();
             $quantity                        = $this->owner->getProductQuantityInCart();
             $price                           = false;
-            $filter                          = array(
+            $filter                          = [
                 'SilvercartProductID' => $this->owner->ID,
-            );
+            ];
             $this->owner->extend('updateGraduatedPriceFilter', $filter);
             $graduatedPrices                 = SilvercartGraduatedPrice::get()->filter($filter)->where('"minimumQuantity" <= ' . $quantity);
-            $graduatedPricesForMembersGroups = new ArrayList();
+            $graduatedPricesForMembersGroups = ArrayList::create();
 
-            if ($member) {
-                if ($graduatedPrices) {
-                    foreach ($graduatedPrices as $graduatedPrice) {
-                        if ($graduatedPrice->CustomerGroups() &&
-                            $graduatedPrice->CustomerGroups()->exists() &&
-                            $member->inGroups($graduatedPrice->CustomerGroups())) {
-
-                            if ($this->isPriceQualified($graduatedPrice, $member)) {
-                                $graduatedPricesForMembersGroups->push($graduatedPrice);
-                            }
-                        }
+            if ($member instanceof Member
+             && $member->exists()
+             && $graduatedPrices->exists()
+            ) {
+                foreach ($graduatedPrices as $graduatedPrice) {
+                    if ($graduatedPrice->CustomerGroups()
+                     && $graduatedPrice->CustomerGroups()->exists()
+                     && $member->inGroups($graduatedPrice->CustomerGroups())
+                     && $this->isPriceQualified($graduatedPrice, $member)
+                    ) {
+                        $graduatedPricesForMembersGroups->push($graduatedPrice);
                     }
                 }
-            } else {
-                if ($graduatedPrices) {
-                    foreach ($graduatedPrices as $graduatedPrice) {
-                        if ($graduatedPrice->CustomerGroups() &&
-                            $graduatedPrice->CustomerGroups()->exists()) {
-
-                            if ($graduatedPrice->CustomerGroups()->find('Code', 'anonymous')) {
-                                if ($this->isPriceQualified($graduatedPrice)) {
-                                    $graduatedPricesForMembersGroups->push($graduatedPrice);
-                                }
-                            }
-                        }
+            } elseif ($graduatedPrices->exists()) {
+                foreach ($graduatedPrices as $graduatedPrice) {
+                    if ($graduatedPrice->CustomerGroups()
+                     && $graduatedPrice->CustomerGroups()->exists()
+                     && $graduatedPrice->CustomerGroups()->find('Code', 'anonymous')
+                     && $this->isPriceQualified($graduatedPrice)
+                    ) {
+                        $graduatedPricesForMembersGroups->push($graduatedPrice);
                     }
                 }
             }
@@ -134,7 +131,6 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
             }
             $this->graduatedPriceForCustomersGroups = $price;
         }
-
         return $this->graduatedPriceForCustomersGroups;
     }
 
@@ -143,47 +139,43 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      *
      * @return ArrayList
      */
-    public function getGraduatedPricesForCustomersGroups() {
+    public function getGraduatedPricesForCustomersGroups()
+    {
         if ($this->graduatedPricesForCustomersGroups === null) {
             $member                          = SilvercartCustomer::currentUser();
-            $graduatedPricesForMembersGroups = new ArrayList();
-            $filter                          = array(
+            $graduatedPricesForMembersGroups = ArrayList::create();
+            $filter                          = [
                 'SilvercartProductID' => $this->owner->ID,    
-            );
+            ];
             $this->owner->extend('updateGraduatedPricesFilter', $filter);
-            $graduatedPrices                 = SilvercartGraduatedPrice::get()->filter($filter)->sort('minimumQuantity', 'ASC');
+            $graduatedPrices = SilvercartGraduatedPrice::get()->filter($filter)->sort('minimumQuantity', 'ASC');
 
-            if ($member) {
-                if ($graduatedPrices) {
-                    foreach ($graduatedPrices as $graduatedPrice) {
-                        if ($graduatedPrice->CustomerGroups() &&
-                            $graduatedPrice->CustomerGroups()->exists() &&
-                            $member->inGroups($graduatedPrice->CustomerGroups())) {
-
-                            if ($this->isPriceQualified($graduatedPrice, $member)) {
-                                $graduatedPricesForMembersGroups->push($graduatedPrice);
-                            }
-                        }
+            if ($member instanceof Member
+             && $member->exists()
+             && $graduatedPrices->exists()
+            ) {
+                foreach ($graduatedPrices as $graduatedPrice) {
+                    if ($graduatedPrice->CustomerGroups()
+                     && $graduatedPrice->CustomerGroups()->exists()
+                     && $member->inGroups($graduatedPrice->CustomerGroups())
+                     && $this->isPriceQualified($graduatedPrice, $member)
+                    ) {
+                        $graduatedPricesForMembersGroups->push($graduatedPrice);
                     }
                 }
-            } else {
-                if ($graduatedPrices) {
-                    foreach ($graduatedPrices as $graduatedPrice) {
-                        if ($graduatedPrice->CustomerGroups()->exists()) {
-
-                            if ($graduatedPrice->CustomerGroups()->find('Code', 'anonymous')) {
-                                if ($this->isPriceQualified($graduatedPrice)) {
-                                    $graduatedPricesForMembersGroups->push($graduatedPrice);
-                                }
-                            }
-                        }
+            } elseif ($graduatedPrices->exists()) {
+                foreach ($graduatedPrices as $graduatedPrice) {
+                    if ($graduatedPrice->CustomerGroups()->exists()
+                     && $graduatedPrice->CustomerGroups()->find('Code', 'anonymous')
+                     && $this->isPriceQualified($graduatedPrice)
+                    ) {
+                        $graduatedPricesForMembersGroups->push($graduatedPrice);
                     }
                 }
             }
             $this->owner->extend('updateGraduatedPricesForCustomersGroups', $graduatedPricesForMembersGroups, $member);
             $this->graduatedPricesForCustomersGroups = $graduatedPricesForMembersGroups->sort('minimumQuantity', 'ASC');
         }
-
         if ($this->graduatedPricesForCustomersGroups->exists()) {
             return $this->graduatedPricesForCustomersGroups;
         } else {
@@ -204,13 +196,14 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      * @author Sebastian Diel <sdiel@pixeltricks.de>
      * @since 20.12.2017
      */
-    public function isPriceQualified(SilvercartGraduatedPrice $graduatedPrice, Member $member = null) {
+    public function isPriceQualified(SilvercartGraduatedPrice $graduatedPrice, Member $member = null) : bool
+    {
         $isPriceQualified = true;
-        $result = $graduatedPrice->extend('updateIsPriceQualified', $member);
-        if (is_array($result) &&
-            !empty($result) &&
-            in_array(false, $result, true)) {
-            
+        $result           = $graduatedPrice->extend('updateIsPriceQualified', $member);
+        if (is_array($result)
+         && !empty($result)
+         && in_array(false, $result, true)
+        ) {
             $isPriceQualified = false;
         }
         return $isPriceQualified;
@@ -221,26 +214,23 @@ class SilvercartGraduatedPriceProduct extends DataExtension {
      * the positions quantity will be returned. If the product is not in the cart
      * yet 1 will be returned.
      * 
-     * @return integer
-     * 
-     * @author Sebastian Diel <sdiel@pixeltricks.de>
-     *         Roland Lehmann <rlehmann@pixeltricks.de>
-     * @since 23.09.2016
+     * @return int
      */
-    public function getProductQuantityInCart() {
+    public function getProductQuantityInCart() : int
+    {
         $quantity   = 1;
         $member     = SilvercartCustomer::currentUser();
-        if ($member instanceof Member &&
-            $member->exists()) {
+        if ($member instanceof Member
+         && $member->exists()
+        ) {
             $whereClause    = sprintf('"SilvercartProductID" = \'%s\' AND "SilvercartShoppingCartID" = \'%s\'', $this->owner->ID, $member->SilvercartShoppingCartID);
             $position       = SilvercartShoppingcartPosition::get()->where($whereClause)->first();
-            if ($position instanceof SilvercartShoppingcartPosition &&
-                $position->exists()) {
-                $quantity = $position->Quantity;
+            if ($position instanceof SilvercartShoppingcartPosition
+             && $position->exists()
+            ) {
+                $quantity = (int) $position->Quantity;
             }
         }
         return $quantity;
     }
-    
 }
-
